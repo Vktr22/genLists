ce943408eb31fc3df84c7d44623e1260a0b0dacb
Vktr22 <nds.viktoria@gmail.com>
2026-01-29 15:13:54 +0100

    genHalmaz

---
+++ src/main/java/GenerikusHalmaz.java
@@ -0,0 +1,13 @@
+import java.util.Set;
+
+public class GenerikusHalmaz {
+    //halmaz tulajdonsága: nem lehet benne 2 egyforma ---> a halmaz tud ilyet! halmaz adatszerkezet--->Set
+        //halmaz = new HashSet<>();
+        //van generikus és nem generikus verziója-- gen kell mert az tipusbiztos + modernebb
+    private Set<Integer> halmaz;
+
+    //feladat: szelvény generátor- 5ös lottó ->
+        //tesztek erre: van e benne 5szám? minden szám [1;95] van e?
+
+    
+}
--- src/main/java/Osztalyzatok.java
+++ src/main/java/Osztalyzatok.java
@@ -1,22 +1,44 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Osztalyzatok {
 
     public static void main(String[] args) {
         //feladat: nevek jegyek 2lista nem privat adattag lista ne legyen módosítható + írj rá kiíró metódust, statik, +átadható legyen + legyen benne main
-
-        List<String> nevek = List.of("Ede", "Pál");
-        List<Double> atlagok = List.of(3.14, 2.65);
+        //immutable adat - lista nem modosithato
+       // List<String> nevek = List.of("Ede", "Pál");
+       // List<Double> atlagok = List.of(3.14, 2.65);
         //feladat: listat atadni aminek az elso elemet at lehet irni
 
 //        hozzaad(nevek, "Pisti");
 //        hozzaad(atlagok, 4.56);
 
+        List<String> nevek = Arrays.asList("Ede", "Pál");
+        List<Double> atlagok = Arrays.asList(3.14, 2.65);
+
         listaKiir(atlagok);
         listaKiir(nevek);
 
+        atir(nevek, "Pisti");
+        atir(atlagok, 2.11);
+
+        listaKiirMetodusReferenciaval(nevek);
+        listaKiirMetodusReferenciaval(atlagok);
+
     }
+
+    static <T> void atir(List<T> lista, T ujErtek) {
+        if (lista == null) {
+            throw new IllegalArgumentException("A lista nem lehet null.");
+        }
+        if (lista.isEmpty()) {
+            System.out.println("A lista üres, nincs mit átírni.");
+            return;
+        }
+        lista.set(0, ujErtek);
+    }
+
+
     /*static void listaKiir(List<Double> atlagok) {       //ez
         for (Double elem : lista) {
             System.out.println(elem + " ");
@@ -31,7 +53,7 @@ public class Osztalyzatok {
         System.out.println();
 
     }*/
-    static void listaKiir(List<?> atlagok) {
+    static void listaKiir(List<?> lista) {
         for (Object elem : lista) {
             System.out.println(elem + " ");
         }
@@ -40,6 +62,16 @@ public class Osztalyzatok {
     }
     static <T> void hozzaad(List<T> lista, T elem){             //T valamilyen típus lesz majd ---típus paraméter lista-val List<T>--> generikus lista es fontos, h nagybetűs, csomagos típust(pl Integer) vesz át, nem egyszerű típusokat(pl int)
         lista.add(elem);
+    }
+
+    static <T> void listaKiirMetodusReferenciaval(List<T> lista){
+        //hagyományos metódus paraméterrel:
+        /*for (T elem : lista){
+            System.out.println(elem);
+        }*/
+
+        //metódus referenciaval:
+        lista.forEach(System.out::println);
     }
 
 
